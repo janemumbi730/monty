918c9829cd94047f8d2030b6b9ddbea0f7b1a357
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-06-20 13:25:41 +0300

    functions2.c

---
+++ functions2.c
@@ -0,0 +1,79 @@
+#include "monty.h"
+
+/**
+ * op_pall - prints stack
+ * @stack: double pointer
+ * @line_number: line number
+ * Return: void
+ */
+void op_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *x = *stack;
+
+	(void)line_number;
+
+	while (x != NULL)
+	{
+		printf("%d\n", x->n);
+		x = x->next;
+	}
+}
+
+/**
+ * handle_push - pushes stack
+ * @stack: double pointer
+ * @line_number: line number
+ * Return: void
+ */
+void handle_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *x;
+  
+	x = malloc(sizeof(stack_t));
+	if (x == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	x->n = line_number;
+	x->prev = NULL;
+	x->next = NULL;
+
+	if (*stack != NULL)
+		(*stack)->prev = x;
+	x->next = *stack;
+	*stack = x;
+}
+
+/**
+ * op_swap - swaps two elements of stack
+ * @stack: double pointer
+ * @line_number: line number
+ * Return: void
+ */
+void op_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *up;
+	stack_t *next;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	up = *stack;
+	next = (*stack)->next;
+
+	up->prev = next;
+	up->next = next->next;
+
+	up->prev = NULL;
+	up->next = up;
+
+	if (up->next != NULL)
+		up->next->prev = up;
+
+	*stack = next;
+}
