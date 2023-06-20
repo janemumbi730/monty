0a485fd84c14f324e5bb1720dc2b0a62cf52721d
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-06-20 13:29:51 +0300

    Updated monty.h

--- monty.h
+++ monty.h
@@ -46,4 +46,10 @@ void op_sub(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
 
+/* functions2 */
+void op_push(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+
+
 #endif
