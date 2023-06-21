d9f7748bccb780b41e4cfbb1659d1e73f85963bb
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-06-21 10:28:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -63,36 +63,43 @@ void free_vrall(void)
  */
 int main(int ac, char *av[])
 {
-	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fd;
-	size_t x = 256;
-	ssize_t y = 0;
-	char *z[2] = {NULL, NULL};
+    void (*f)(stack_t **stack, unsigned int line_number);
+    FILE *fd;
+    size_t x = 256;
+    char *line = NULL;
+    ssize_t y = 0;
+    char *z[2] = {NULL, NULL};
 
-	fd = input_scanner(ac, av);
-	start_vrall(fd);
-	y = getline(&vrall.buf, &x, fd);
-	while (y != -1)
-	{
-		z[0] = my_strtok(vrall.buf, " \t\n");
-		if (z[0] && z[0][0] != '#')
-		{
-			f = retrive_ops(z[0]);
-			if (!f)
-			{
-				dprintf(2, "L%u: ", vrall.cline);
-				dprintf(2, "unknown instruction %s\n", z[0]);
-				free_vrall();
-				exit(EXIT_FAILURE);
-			}
-			vrall.args = my_strtok(NULL, " \t\n");
-			f(&vrall.h, vrall.cline);
-		}
-		y = getline(&vrall.buf, &x, fd);
-		vrall.cline++;
-	}
+    fd = input_scanner(ac, av);
+    start_vrall(fd);
+    line = (char*)malloc(sizeof(char) * x); // Allocate memory for line
+    if (!line)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    while ((y = fgets(line, x, fd)) != NULL)
+    {
+        z[0] = my_strtok(line, " \t\n");
+        if (z[0] && z[0][0] != '#')
+        {
+            f = retrive_ops(z[0]);
+            if (!f)
+            {
+                fprintf(stderr, "L%u: ", vrall.cline);
+                fprintf(stderr, "unknown instruction %s\n", z[0]);
+                free(line);
+                free_vrall();
+                exit(EXIT_FAILURE);
+            }
+            vrall.args = my_strtok(NULL, " \t\n");
+            f(&vrall.h, vrall.cline);
+        }
+        vrall.cline++;
+    }
 
-	free_vrall();
+    free(line);
+    free_vrall();
 
-	return (0);
+    return 0;
 }
