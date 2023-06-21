8656e384eb233eeea30e35b214dc9c12ac76f31e
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-06-21 12:43:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "monty.h"
+
 overrall_t vrall;
+
 /**
  * start_vrall - starts the universal variables
  * @fd: file descrp
@@ -26,20 +28,20 @@ FILE *input_scanner(int ac, char *av[])
 	FILE *fd;
 
 	if (ac == 1 || ac > 2)
-{
-    fprintf(stderr, "USAGE: monty file\n");
-    exit(EXIT_FAILURE);
-}
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-fd = fopen(av[1], "r");
+	fd = fopen(av[1], "r");
 
-if (fd == NULL)
-{
-    fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-    exit(EXIT_FAILURE);
-}
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
 
-return (fd);
+	return (fd);
 }
 
 /**
@@ -61,44 +63,44 @@ void free_vrall(void)
  */
 int main(int ac, char *av[])
 {
-    void (*f)(stack_t **stack, unsigned int line_number);
-    FILE *fd;
-    size_t x = 256;
-    char *line = NULL;
-    char *y = NULL;
-    char *z[2] = {NULL, NULL};
+	void (*f)(stack_t **stack, unsigned int line_number);
+	FILE *fd;
+	size_t x = 256;
+	char *line = NULL;
+	char *y = NULL;
+	char *z[2] = {NULL, NULL};
 
-    fd = input_scanner(ac, av);
-    start_vrall(fd);
-    line = (char*)malloc(sizeof(char) * x);
-    if (!line)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    while ((y = fgets(line, x, fd)) != NULL)
-    {
-        z[0] = my_strtok(line, " \t\n");
-        if (z[0] && z[0][0] != '#')
-        {
-            f = retrive_ops(z[0]);
-            if (!f)
-            {
-                fprintf(stderr, "L%u: ", vrall.cline);
-                fprintf(stderr, "unknown instruction %s\n", z[0]);
-                free(line);
-                free_vrall();
-                exit(EXIT_FAILURE);
-            }
-            vrall.args = my_strtok(NULL, " \t\n");
-            f(&vrall.h, vrall.cline);
-        }
-        vrall.cline++;
-    }
+	fd = input_scanner(ac, av);
+	start_vrall(fd);
+	line = (char *)malloc(sizeof(char) * x);
+	if (!line)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	while ((y = fgets(line, x, fd)) != NULL)
+	{
+		z[0] = my_strtok(line, " \t\n");
+		if (z[0] && z[0][0] != '#')
+	{
+		f = retrive_ops(z[0]);
+		if (!f)
+		{
+			fprintf(stderr, "L%u: ", vrall.cline);
+			fprintf(stderr, "unknown instruction %s\n", z[0]);
+			free(line);
+			free_vrall();
+			exit(EXIT_FAILURE);
+		}
+		vrall.args = my_strtok(NULL, " \t\n");
+		f(&vrall.h, vrall.cline);
+	}
+	vrall.cline++;
+	}
 
-    free(line);
-    free_vrall();
+	free(line);
+	free_vrall();
 
-    return 0;
+	return (0);
 }
 
