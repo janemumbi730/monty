c79b86cd2a4a48dee9015cf1633b4acd9866c205
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-06-21 10:11:08 +0300

    Update functions4.c

--- functions4.c
+++ functions4.c
@@ -64,21 +64,22 @@ void op_multiply(stack_t **stack, unsigned int line_number)
 	int w = 0;
 	stack_t *q = NULL;
 
-	q = *stack;
+q = *stack;
 
-	for (; q != NULL; q = q->next, w++)
-		;
+for (; q != NULL; q = q->next, w++)
+    ;
 
-	if (w < 2)
-	{
-		dprintf(2, "L%u: can't mul, stack too short\n", line_number);
-		free_vrall();
-		exit(EXIT_FAILURE);
-	}
+if (w < 2)
+{
+    fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+    free_vrall();
+    exit(EXIT_FAILURE);
+}
 
-	q = (*stack)->next;
-	q->n *= (*stack)->n;
-	op_pop(stack, line_number);
+q = (*stack)->next;
+q->n *= (*stack)->n;
+op_pop(stack, line_number);
+
 }
 /**
  * my_search - reviews if char is inside string
